f725008dacef125d5648751a86ab7cb62ff719f8
Christophe Benz <christophe.benz@gmail.com>
2016-03-01 18:00:43 +0100

    Update path

--- m_language_parser/scripts/check_grammar.sh
+++ m_language_parser/scripts/check_grammar.sh
@@ -4,7 +4,7 @@
 
 SCRIPT_DIR=$(dirname $(readlink -f "$BASH_SOURCE"))
 SCRIPT="$SCRIPT_DIR/m_source_file_to_json_ast.py"
-GRAMMAR_FILE=`realpath $SCRIPT_DIR/../data/m_language.cleanpeg`
+GRAMMAR_FILE=`realpath $SCRIPT_DIR/../../data/m_language.cleanpeg`
 
 EXTRACT_GRAMMAR_RULES="grep -o -E '^\w+' $GRAMMAR_FILE | grep -v EOF | sort"
 EXTRACT_VISIT_FUNCTIONS_RULES="grep -Po '(?<=def visit_).+(?=\(.+:)' $SCRIPT | grep -v _default__ | sort"
